b2065787d40c7edb085e15337b614b6e0247122c
N1kunj1998 <njkhakhkhar@gmail.com>
2024-03-17 01:48:38 +0530

    making page structure and setting up routing with lazy imports

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { lazy } from 'react';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
+
+const Home = lazy(() => import("./pages/Home"));
+const Login = lazy(() => import("./pages/Login"));
+const Chat = lazy(() => import("./pages/Chat"));
+const Group = lazy(() => import("./pages/Group"));
 
 const App = () => {
   return (
     <BrowserRouter>
     <Routes>
-      <Route path="/" element={<div>App</div>} />
+      <Route path="/" element={<Home/>} />
+      <Route path="/login" element={<Login/>} />
+      <Route path="/chat/:chatId" element={<Chat/>} />
+      <Route path="/Group" element={<Group/>} />
     </Routes>
     </BrowserRouter>
   )
